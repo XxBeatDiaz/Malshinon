82b05ed25f298e4569ba6a1ef454f26d05ac2ceb
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-09 17:27:25 +0300

    Interface for Dals

---
+++ Dals/InterfaceDals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Models
+{
+    public interface InterfaceDals
+    {
+        public void Add();
+        public object GetById();
+        public List<object> GetList();
+        public void Delete();
+    }
+}
