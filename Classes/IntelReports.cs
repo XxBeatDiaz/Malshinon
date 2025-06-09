4bb4f229429dd955aec567a05ac5176ce78dd015
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-09 15:37:12 +0300

    Intel reports class

---
+++ Classes/IntelReports.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Malshinon.Classes
+{
+    class IntelReports
+    {
+        int Id;
+        int ReporterId;
+        int TargetId;
+        string Content;
+        DateTime Timestamp;
+    }
+}
