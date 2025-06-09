d2284362e56a7354ce9c6090ece6b2ce4b1d2418
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-09 15:30:11 +0300

    People class

---
+++ Classes/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Classes
+{
+    public class People
+    {
+        int Id;
+        string FirstName, LastName;
+        string SecretCode;
+        string TypeOfPeople;
+        int NumReports;
+        int NumMentions;
+
+        public void PrintPeople()
+        {
+            Console.WriteLine($"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}, TypeOfPeople: {TypeOfPeople}, NumReports: {NumReports}, NumNentions: {NumMentions}");
+        }
+    }
+}
