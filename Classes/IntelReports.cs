4abaa6cfc15344d3f74892fd8d4060b77c830dd8
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-10 09:38:02 +0300

    Update the Intel reports class: Add Get/Set Print function

--- Classes/IntelReports.cs
+++ Classes/IntelReports.cs
@@ -8,12 +8,17 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace Malshinon.Models
 {
-    class IntelReports
+    public class IntelReports
     {
-        int Id;
-        int ReporterId;
-        int TargetId;
-        string Content;
-        DateTime Timestamp;
+        int Id { get; set; }
+        int ReporterId { get; set; }
+        int TargetId { get; set; }
+        string Content { get; set; }
+        DateTime Timestamp { get; set; }
+
+        public void ShowIntelReports()
+        {
+            Console.WriteLine($":{Id},:{ReporterId},:{TargetId},:{Content},:{Timestamp}");
+        }
     }
 }
